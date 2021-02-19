84288b773cca9e3816301eab289937722a2ecd83
Sami <530240@student.glu.nl>
2021-02-19 10:47:44 +0100

    curve feever update

---
+++ Unity/Assets/Scripts/MP/PlayerSpawner.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class PlayerSpawner : MonoBehaviourPunCallbacks
+{
+    [SerializeField] private string m_playerPrefabStr = string.Empty;
+
+    private void Awake()
+    {
+        if (PhotonNetwork.InRoom)
+        {
+            int i = 0;
+            foreach (var item in PhotonNetwork.CurrentRoom.Players)
+            {
+                i = item.Key;
+                break;
+            }
+
+            GameObject go = PhotonNetwork.Instantiate(m_playerPrefabStr, new Vector3(0, i, 0), Quaternion.identity);
+            go.name += PhotonNetwork.NickName;
+        }
+
+        Destroy(gameObject);
+    }
+}
---
+++ Unity/Assets/Scripts/SliderToText.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class SliderToText : MonoBehaviour
+{
+    [SerializeField] TMP_Text text;
+    private Slider slider;
+
+    private void Start()
+    {
+        if (text.gameObject == null)
+        {
+            text = GetComponentInChildren<TMP_Text>();
+        }
+
+        slider = GetComponent<Slider>();
+
+        slider.onValueChanged.AddListener(delegate { ChangeText(); });
+
+        ChangeText();
+    }
+
+    void ChangeText()
+    {
+        text.text = slider.value.ToString();
+    }
+}
